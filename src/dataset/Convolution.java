8bf4fd5f52a19631b3e79d6559959a44ec41c4af
alex.zabashta <alex.zabashta>
2019-05-12 02:06:06 +0300

    add forward to convolution

--- src/dataset/Convolution.java
+++ src/dataset/Convolution.java
@@ -7,33 +7,33 @@ import core.DiffStruct;
 
 public class Convolution<H, V> implements DiffStruct<Triple<double[][][], double[], double[]>, Convolution<H, V>.Memory, double[]> {
 
-    class Memory {
+    public class Memory {
         final Node root;
-        final int rows, columns;
-        final int numHorzBoundVar, numVertBoundVar;
+        final int rows, cols;
 
-        public Memory(Node root, int rows, int columns, int numHorzBoundVar, int numVertBoundVar) {
+        Memory(Node root, int rows, int cols) {
             this.root = root;
             this.rows = rows;
-            this.columns = columns;
-            this.numHorzBoundVar = numHorzBoundVar;
-            this.numVertBoundVar = numVertBoundVar;
+            this.cols = cols;
         }
 
-        public Triple<double[][][], double[], double[]> backward(double[] dy) {
-            double[][][] dx = new double[rows][columns][];
+        void normalize(double[] array, double scale) {
+            for (int i = 0; i < array.length; i++) {
+                array[i] *= scale;
+            }
+        }
+
+        Triple<double[][][], double[], double[]> backward(double[] dy) {
+            double[][][] dx = new double[rows][cols][];
             double[] dh = new double[numHorzBoundVar];
             double[] dv = new double[numVertBoundVar];
 
-            int cnt = root.backward(dy, dx, dh, dv, 0);
+            int cnt = root.backward(dy, dx, dh, dv);
 
             if (cnt > 0) {
-                for (int i = 0; i < numHorzBoundVar; i++) {
-                    dh[i] /= cnt;
-                }
-                for (int i = 0; i < numVertBoundVar; i++) {
-                    dv[i] /= cnt;
-                }
+                double scale = 1.0 / cnt;
+                normalize(dh, scale);
+                normalize(dv, scale);
             }
 
             return Triple.of(dx, dh, dv);
@@ -41,73 +41,24 @@ public class Convolution<H, V> implements DiffStruct<Triple<double[][][], double
 
     }
 
-    class Encode extends Node {
-        public final int row, col;
-
-        public Encode(double[] values, int row, int col) {
-            super(values);
-            this.row = row;
-            this.col = col;
-        }
-
-        @Override
-        public int backward(double[] dy, double[][][] dx, double[] sdh, double[] sdv, int cnt) {
-            dx[row][col] = dy;
-            return cnt;
-        }
-
-    }
-
     abstract class Node {
-        public final double[] values;
+        final double[] values;
 
-        public Node(double[] values) {
+        Node(double[] values) {
             this.values = values;
         }
 
-        public abstract int backward(double[] dy, double[][][] dx, double[] sdh, double[] sdv, int cnt);
+        abstract int backward(double[] dy, double[][][] dx, double[] sdh, double[] sdv);
 
-    }
-
-    class HorzFold extends Fold {
-        final H horzMem;
-
-        public HorzFold(Node fst, Node snd, H horzMem, double[] values) {
-            super(fst, snd, values);
-            this.horzMem = horzMem;
-        }
-
-        @Override
-        double[] backward(double[] dy, double[] sdh, double[] sdv) {
-            Pair<double[], double[]> dxh = horzFold.backward(horzMem, dy);
-            addWithNormalization(sdh, dxh.getRight());
-            return dxh.getLeft();
-        }
-    }
-
-    class VertFold extends Fold {
-        final V vertMem;
-
-        public VertFold(Node fst, Node snd, V vertMem, double[] values) {
-            super(fst, snd, values);
-            this.vertMem = vertMem;
-        }
-
-        @Override
-        double[] backward(double[] dy, double[] sdh, double[] sdv) {
-            Pair<double[], double[]> dxv = vertFold.backward(vertMem, dy);
-            addWithNormalization(sdv, dxv.getRight());
-            return dxv.getLeft();
-        }
     }
 
     abstract class Fold extends Node {
-        final Node fst, snd;
+        final Node first, secnd;
 
-        public Fold(Node fst, Node snd, double[] values) {
+        Fold(Node first, Node secnd, double[] values) {
             super(values);
-            this.fst = fst;
-            this.snd = snd;
+            this.first = first;
+            this.secnd = secnd;
         }
 
         void addWithNormalization(double[] src, double[] dst) {
@@ -124,7 +75,7 @@ public class Convolution<H, V> implements DiffStruct<Triple<double[][][], double
         abstract double[] backward(double[] dy, double[] sdh, double[] sdv);
 
         @Override
-        public int backward(double[] dy, double[][][] dInput, double[] sdh, double[] sdv, int cnt) {
+        int backward(double[] dy, double[][][] dInput, double[] sdh, double[] sdv) {
             double[] dx = backward(dy, sdh, sdv);
 
             double[] da = new double[depth];
@@ -133,30 +84,90 @@ public class Convolution<H, V> implements DiffStruct<Triple<double[][][], double
             System.arraycopy(dx, 0, da, 0, depth);
             System.arraycopy(dx, depth, db, 0, depth);
 
-            cnt = fst.backward(da, dInput, sdh, sdv, cnt + 1);
-            cnt = snd.backward(db, dInput, sdh, sdv, cnt + 0);
-            return cnt;
+            return 1 + first.backward(da, dInput, sdh, sdv) + secnd.backward(db, dInput, sdh, sdv);
         }
 
     }
 
-    // TODO set variables
-    final DiffStruct<Pair<double[], double[]>, H, double[]> horzFold = null;
-    final DiffStruct<Pair<double[], double[]>, V, double[]> vertFold = null;
+    class HorzFold extends Fold {
+        final H horzMem;
 
-    final int depth = 0;
+        HorzFold(Node first, Node secnd, H horzMem, double[] values) {
+            super(first, secnd, values);
+            this.horzMem = horzMem;
+        }
 
-    public Fold buildNode(boolean horizontal, double[] foldBoundVar, Node left, Node right) {
+        @Override
+        double[] backward(double[] dy, double[] sdh, double[] sdv) {
+            Pair<double[], double[]> dxh = horzFold.backward(horzMem, dy);
+            addWithNormalization(sdh, dxh.getRight());
+            return dxh.getLeft();
+        }
+    }
+
+    class VertFold extends Fold {
+        final V vertMem;
+
+        VertFold(Node first, Node secnd, V vertMem, double[] values) {
+            super(first, secnd, values);
+            this.vertMem = vertMem;
+        }
+
+        @Override
+        double[] backward(double[] dy, double[] sdh, double[] sdv) {
+            Pair<double[], double[]> dxv = vertFold.backward(vertMem, dy);
+            addWithNormalization(sdv, dxv.getRight());
+            return dxv.getLeft();
+        }
+    }
+
+    class DatasetCell extends Node {
+        final int row, col;
+
+        DatasetCell(double[] values, int row, int col) {
+            super(values);
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        int backward(double[] dy, double[][][] dx, double[] sdh, double[] sdv) {
+            dx[row][col] = dy;
+            return 0;
+        }
+
+    }
+
+    public final int depth, numHorzBoundVar, numVertBoundVar;
+    public final boolean symmetric;
+    public final DiffStruct<Pair<double[], double[]>, H, double[]> horzFold;
+    public final DiffStruct<Pair<double[], double[]>, V, double[]> vertFold;
+
+    public Convolution(boolean symmetric, int depth, DiffStruct<Pair<double[], double[]>, H, double[]> horzFold, int numHorzBoundVar, DiffStruct<Pair<double[], double[]>, V, double[]> vertFold, int numVertBoundVar) {
+        this.symmetric = symmetric;
+        this.depth = depth;
+        this.horzFold = horzFold;
+        this.numHorzBoundVar = numHorzBoundVar;
+        this.vertFold = vertFold;
+        this.numVertBoundVar = numVertBoundVar;
+    }
+
+    @Override
+    public Triple<double[][][], double[], double[]> backward(Memory memory, double[] deltaOutput) {
+        return memory.backward(deltaOutput);
+    }
+
+    Fold buildNode(boolean horizontal, double[] foldBoundVar, Node first, Node secnd) {
         double[] x = new double[depth * 2];
-        System.arraycopy(left.values, 0, x, 0, depth);
-        System.arraycopy(right.values, 0, x, depth, depth);
+        System.arraycopy(first.values, 0, x, 0, depth);
+        System.arraycopy(secnd.values, 0, x, depth, depth);
 
         if (horizontal) {
             Pair<H, double[]> result = horzFold.forward(Pair.of(x, foldBoundVar));
-            return new HorzFold(left, right, result.getLeft(), result.getRight());
+            return new HorzFold(first, secnd, result.getLeft(), result.getRight());
         } else {
             Pair<V, double[]> result = vertFold.forward(Pair.of(x, foldBoundVar));
-            return new VertFold(left, right, result.getLeft(), result.getRight());
+            return new VertFold(first, secnd, result.getLeft(), result.getRight());
         }
     }
 
@@ -167,30 +178,100 @@ public class Convolution<H, V> implements DiffStruct<Triple<double[][][], double
         double[] horzBoundVar = input.getMiddle();
         double[] vertBoundVar = input.getRight();
 
-        final int rows = dataset.length, columns = dataset[0].length; // TODO check length > 0!
+        final int rows = dataset.length, cols = dataset[0].length;
 
         @SuppressWarnings("unchecked")
-        Node[][] nodes = (Node[][]) new Object[rows][columns];
+        Node[][] nodes = (Node[][]) new Object[rows][cols];
 
         for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
-                nodes[row][col] = new Encode(dataset[row][col], row, col);
+            for (int col = 0; col < cols; col++) {
+                nodes[row][col] = new DatasetCell(dataset[row][col], row, col);
             }
         }
 
-        int curRows = rows, curColumns = columns;
+        int curRows = rows, curCols = cols;
 
-        while (curRows > 1 || curColumns > 1) {
-            // TODO implement forward
+        while (curRows > 1 || curCols > 1) {
+            double rowSim = Double.POSITIVE_INFINITY, colSim = Double.POSITIVE_INFINITY;
+            int rowA = 0, rowB = 1;
+            int colA = 0, colB = 1;
+
+            for (int rowU = 1; rowU < curRows; rowU++) {
+                for (int rowD = symmetric ? 0 : (rowU - 1); rowD < rowU; rowD++) {
+                    double sim = 0;
+                    double sumD = 0, sumU = 0;
+                    for (int col = 0; col < curCols; col++) {
+                        for (int i = 0; i < depth; i++) {
+                            sumD += nodes[rowD][col].values[i];
+                            sumU += nodes[rowU][col].values[i];
+                            double diff = nodes[rowD][col].values[i] - nodes[rowU][col].values[i];
+                            sim += diff * diff;
+                        }
+                    }
+
+                    sim /= curCols;
+
+                    if (sim < rowSim) {
+                        rowSim = sim;
+                        rowA = rowD;
+                        rowB = rowU;
+
+                        if (symmetric && sumD > sumU) {
+                            rowA ^= rowB;
+                            rowB ^= rowA;
+                            rowA ^= rowB;
+                        }
+                    }
+                }
+            }
+
+            for (int colR = 1; colR < curCols; colR++) {
+                for (int colL = symmetric ? 0 : (colR - 1); colL < colR; colL++) {
+                    double sim = 0;
+                    double sumL = 0, sumR = 0;
+                    for (int row = 0; row < curRows; row++) {
+                        for (int i = 0; i < depth; i++) {
+                            sumL += nodes[row][colL].values[i];
+                            sumR += nodes[row][colR].values[i];
+                            double diff = nodes[row][colL].values[i] - nodes[row][colR].values[i];
+                            sim += diff * diff;
+                        }
+                    }
+
+                    sim /= curRows;
+
+                    if (sim < colSim) {
+                        colSim = sim;
+                        colA = colL;
+                        colB = colR;
+
+                        if (symmetric && sumL > sumR) {
+                            colA ^= colB;
+                            colB ^= colA;
+                            colA ^= colB;
+                        }
+                    }
+                }
+            }
+
+            if (colSim < rowSim) {
+                --curCols;
+                for (int row = 0; row < curRows; row++) {
+                    nodes[row][colA] = buildNode(true, horzBoundVar, nodes[row][colA], nodes[row][colB]);
+                    nodes[row][colB] = nodes[row][curCols];
+                }
+            } else {
+                --curRows;
+
+                for (int col = 0; col < curCols; col++) {
+                    nodes[rowA][col] = buildNode(false, vertBoundVar, nodes[rowA][col], nodes[rowB][col]);
+                    nodes[rowB][col] = nodes[curRows][col];
+                }
+            }
         }
 
         Node root = nodes[0][0];
-        return Pair.of(new Memory(root, rows, columns, horzBoundVar.length, vertBoundVar.length), root.values);
-    }
-
-    @Override
-    public Triple<double[][][], double[], double[]> backward(Memory memory, double[] deltaOutput) {
-        return memory.backward(deltaOutput);
+        return Pair.of(new Memory(root, rows, cols), root.values);
     }
 
 }
